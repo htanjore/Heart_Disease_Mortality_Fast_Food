44a28a2b415b1c4caa555644b17b5a72a1d84adf
htanjore <htanjore@gmail.com>
2019-01-17 16:13:00 -0600

    edits

--- Heart_Disease_Mortality_Fast_Food_shiny/server.R
+++ Heart_Disease_Mortality_Fast_Food_shiny/server.R
@@ -22,7 +22,9 @@ server <- function(input, output) {
               id = "NAME",
               textNA = "Unreliable",
               colorNA = "grey",
-              popup.vars = input$var)+
+              popup.vars = c(input$var, "Percent Change in FFR from 09 to 14 per 1000 residents: " = "Percent_Change_FFR_2009_2014",
+                                "Number of FastFood Resturants 09: " = "FFR09",
+                                "Number of FastFood Resturants 14: " = "FFR14"))+
       tm_polygons(col = var, alpha = 0.5, border.col = 'white')+
       tm_bubbles(size="Percent_Change_FFR_2009_2014", col = "Percent_Change_FFR_2009_2014",
                  palette = mycols,
@@ -35,11 +37,12 @@ server <- function(input, output) {
                                 "Number of FastFood Resturants 14: " = "FFR14"))+
       tm_shape(US_states) +
       tm_borders(lwd=2, col = "black", alpha = .25) +
+      tm_style(input$style)+
       tm_layout(title = "Heart Disease Mortality and Fast Food Resturants",
                 title.size = 1.1,
                 title.position = c("center", "top"),bg.color = "gray95")+
-      tm_legend(legend.position = c("right", "bottom"))+
-      tm_style(input$style)
+      tm_legend(legend.position = c("right", "bottom"))
+      
     tmap_leaflet(tm)
     
     
@@ -63,7 +66,9 @@ tm1 <- tm_shape(map_data, projection = 2163)+
             id = "NAME",
             textNA = 'Unreliable',
             colorNA = "grey",
-            popup.vars = input$var)+
+            popup.vars = c(input$var, "Percent Change in FFR from 09 to 14 per 1000 residents: " = "Percent_Change_FFR_2009_2014",
+                           "Number of FastFood Resturants 09: " = "FFR09",
+                           "Number of FastFood Resturants 14: " = "FFR14"))+
       tm_polygons(col = var, alpha = 0.5, border.col = 'white',title=input$var)+
       tm_bubbles(size ="Percent_Change_FFR_2009_2014", col = "Percent_Change_FFR_2009_2014",
                  palette = mycols,
@@ -77,11 +82,12 @@ tm1 <- tm_shape(map_data, projection = 2163)+
       tmap_options(max.categories = 14)+
       tm_shape(US_states) +
       tm_borders(lwd=2, col = "black", alpha = .25) +
+      tm_style(input$style)+
       tm_layout(title = "Heart Disease Mortality and Fast Food Resturants",
                 title.size = 1.1,
                 title.position = c("center", "top"))+
-      tm_legend(legend.position = c("right", "bottom"))+
-  tm_style(input$style)
+      tm_legend(legend.position = c("right", "bottom"))
+ 
     tmap_leaflet(tm1) 
   })
 }
--- Heart_Disease_Mortality_Fast_Food_shiny/ui.R
+++ Heart_Disease_Mortality_Fast_Food_shiny/ui.R
@@ -9,18 +9,18 @@ shinyUI(fluidPage(
       tags$blockquote("About 610,000 people die of heart disease 
                                                in the United States every year–that's 1 in every 4 deaths"),  
       tags$div(
-         HTML(paste(strong(tags$span(style="color:orange", "Data Source:" ,tags$br("CDC Wonder,"),
-                                    tags$p("USDA-Food Environment Atlas"), sep = "")
+         HTML(paste(strong(tags$span(style="color:orange", "Data Source:",tags$p("CDC Wonder:"), tags$em("Diseases of Heart, ICD Code (100-109,l11,l13,l20-151)&"),
+                                    tags$br("USDA-Food Environment Atlas"), sep = "")
                            )))
       ),
-
+        tags$br(""),
                    selectInput("state", 
                             label = "Select a State", 
                             choices = states,
                             selected = 'State_name',selectize = FALSE),
                     selectInput("var", 
                              label = "Select a variable", 
-                             choices = c("Age Adjusted Rate" = 'Age_Adjusted_Rate',
+                             choices = c("Heart Disease Mortality Rate" = 'Age_Adjusted_Rate',
                                          "Percent Adult diabetes 2013" = "Adults_Diabetes_per_13",
                                          "Percent Adult Obese  2013" = "Adult_Obese_Per_13"  ,
                                          "Recreational facilties % 09 14"="Per_Rec_Fac_09-14",
@@ -28,33 +28,30 @@ shinyUI(fluidPage(
                                          "Povery Rate 2015"="Poverty_rate_15"),
                               selectize = FALSE),
                     selectInput("style", 
-                             label = "Select a Style for Background", 
+                             label = "Background Style for Map", 
                              choices = c("natural","white","cobalt", "albatross",
                                         "watercolor","col_blind","beaver", "bw", 
                                         "classic", "watercolor"),
                             selectize = FALSE),
       
       tags$br(""),
-      tags$br(""),
+      
       tags$div(
         HTML(paste(tags$span(style="color:yellow", "NOTE:"), sep = ""))
       ),
-      tags$p(HTML(paste(tags$span(style="color:red", "Click on County:"), sep = "")), "Info about Selected Variable"),
-      tags$p(HTML(paste(tags$span(style="color:red", "Click on Circle:"), sep = "")), "Info about Heart Disease Mortality Rate and Fast Food Restaurants")
+      tags$p(HTML(paste(tags$span(style="color:red", "Click on County:"), sep = "")), "Info about Selected Variable and Fast Food Restaurants"),
+      tags$p(HTML(paste(tags$span(style="color:red", "Click on Circle:"), sep = "")), "Info about Heart Disease Mortality Rate and Fast Food Restaurants"),
+      tags$br(HTML(paste(tags$span(style="color:yellow", "Hari Tanjore"), sep = "")))
      
       ),
-      
      
      
-    
-     
-  
-      mainPanel(
+     mainPanel(
        
               fluidRow(
-               leafletOutput("FFR_tmap",width = "125%")),
+               leafletOutput("FFR_tmap",width = "125%", height=400)),
               fluidRow(
-                leafletOutput("FFR_TN", width = "125%"))
+                leafletOutput("FFR_TN", width = "125%", height=455))
     
       )
 )
